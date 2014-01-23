92d80242b79bd4509efb058e332587426a521fd6
Shay Bromer <shaybromer@Shay-Bromers-MacBook-Pro.local>
2014-01-23 18:59:10 -0500

    Initial Commit

---
+++ SeniorProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SeniorProject
+//
+//  Created by Shay Bromer on 1/23/14.
+//  Copyright (c) 2014 Shay Bromer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
